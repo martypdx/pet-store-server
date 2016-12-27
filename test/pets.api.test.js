98c341b4dbc9cb1ffc5c1820af4c1358563d1299
martypdx <marty@martypdx.com>
2016-12-27 12:16:09 -0800

    add user auth and protection for data routes

--- test/pets.api.test.js
+++ test/pets.api.test.js
@@ -12,20 +12,36 @@ describe('pets', () => {
     const request = chai.request(app);
 
     before(db.drop(connection));
+    
+    let authHeader = null;
+    
+    before(() => request
+        .post('/api/auth/signup')
+        .send({ 
+            username: 'user',
+            password: 'abc'
+        })
+        .then(({ body }) => {
+            assert.isOk(body.token);
+            authHeader = { Authorization: `Bearer ${body.token}` };
+        })
+    );
 
     let store = null;
     
     before(() => request
         .post('/api/stores')
+        .set(authHeader)
         .send({ name: 'best store' })
         .then(({ body }) => {
-            assert.ok(body._id);
             store = body;
+            assert.isOk(store._id);
         })
     );
 
     it('cannot /POST without store id', () => request
         .post('/api/pets')
+        .set(authHeader)
         .send({ name: 'bobby', animal: 'cat' })
         .then(
             () => { throw new Error('unexpected success response'); },
@@ -42,6 +58,7 @@ describe('pets', () => {
         pet.store = store._id;
         return request
             .post('/api/pets')
+            .set(authHeader)
             .send(pet)
             .then(({ body }) => {
                 assert.isOk(body._id);
@@ -54,7 +71,8 @@ describe('pets', () => {
     it('/DELETE pet', () => {
         const url = `/api/pets/${pet._id}`;
         return request.delete(url)
-            .then(() => request.get(url))
+            .set(authHeader)
+            .then(() => request.get(url).set(authHeader))
             .then(
                 () => { throw new Error('unexpected success response'); },
                 res => assert.equal(res.status, 404)
--- test/stores.api.test.js
+++ test/stores.api.test.js
@@ -13,15 +13,30 @@ describe('stores', () => {
 
     const request = chai.request(app);
 
+    let authHeader = null;    
+    before(() => request
+        .post('/api/auth/signup')
+        .send({ 
+            username: 'user',
+            password: 'abc'
+        })
+        .then(({ body }) => {
+            assert.isOk(body.token);
+            authHeader = { Authorization: `Bearer ${body.token}` };
+        })
+    );
+
     let store = { name: 'cute store' };
 
     it('/GET all', () => request
         .get('/api/stores')
+        .set(authHeader)
         .then(res => assert.deepEqual(res.body, []))
     );
 
     it('/POST', () => request
         .post('/api/stores')
+        .set(authHeader)
         .send(store)
         .then(({ body }) => {
             assert.ok(body._id);
@@ -39,12 +54,14 @@ describe('stores', () => {
         pet.store = store._id;
         return request
             .post('/api/pets')
+            .set(authHeader)
             .send(pet)
             .then(({ body }) => pet = body);
     });
 
     it('/GET by id', () => request
         .get(`/api/stores/${store._id}`)
+        .set(authHeader)
         .then(({ body }) =>  {
             assert.equal(body._id, store._id);
             assert.equal(body.name, store.name);
@@ -53,22 +70,23 @@ describe('stores', () => {
         })
     );
 
-    it('/GET all after post contains one item', done => {
-        request
-            .get('/api/stores')
-            .then(({ body }) => {
-                assert.deepEqual(body, [store]);
-                done();
-            })
-            .catch(done);
-    });
+    it('/GET all after post contains one item', () => request
+        .get('/api/stores')
+        .set(authHeader)
+        .then(({ body }) => {
+            assert.deepEqual(body, [store]);
+        })
+    );
 
     it('/DELETE store', () => request
         .delete(`/api/stores/${store._id}`)
+        .set(authHeader)
     );
 
     it('/GET by id gives 404', () => request
-        .get(`/api/stores/${store._id}`).then(
+        .get(`/api/stores/${store._id}`)
+        .set(authHeader)
+        .then(
             () => { throw new Error('unexpected success response'); },
             res => assert.equal(res.status, 404)
         )
@@ -76,6 +94,7 @@ describe('stores', () => {
 
     it('removes associated pets', () => request
         .get('/api/pets')
+        .set(authHeader)
         .then(res => res.body)
         .then(pets => assert.equal(pets.length, 0))
     );
